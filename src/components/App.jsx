6b7c5a62c23619c805d2eeda224a3d4ee3e51cc3
MightyK <KMightyDev@gmail.com>
2021-03-10 12:47:08 -0500

    Basic functionality achieved through the use of destructuring the useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
-    return <div />;
+
+    const [count, setCount] = useState(5);
+    
+    const increase = () => {
+        setCount(count + 1);
+    }
+
+    const decrease = () => {
+        setCount(count - 1);
+    }
+
+    return (
+        <div className='container'>
+            <h1>{count}</h1>
+            <button onClick={increase}>+</button>
+            <button onClick={decrease}>-</button>
+        </div>
+  );
 }
 
 export default App;
